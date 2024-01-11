b9b974e334f357bfcea9f8e5ef19ce2478fd300f
Ahmedkel <ahmedkel@gmail.com>
2024-01-11 16:30:17 +0100

    type-annotated function sum list

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,1 +1,7 @@
 #!/usr/bin/env python3
+"""This module contains the function sum_list(a, b)"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """the function returns sum of list"""
+    return sum(input_list)
