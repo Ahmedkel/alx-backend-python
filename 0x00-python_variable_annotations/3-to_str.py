48bb8b52bd88d62bdead3113243e62c2090c8e67
Ahmedkel <ahmedkel@gmail.com>
2024-01-11 16:11:27 +0100

    type-annotated function to str

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -1,1 +1,7 @@
 #!/usr/bin/env python3
+"""This module contains the function to_str(n)"""
+
+
+def to_str(n: float) -> str:
+    """the function that turns n to str and returns it"""
+    return str(n)
