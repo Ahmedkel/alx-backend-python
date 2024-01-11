7a65fb491fc3f7f90e8dcb68d82d2a58b96fd887
Ahmedkel <ahmedkel@gmail.com>
2024-01-11 17:33:05 +0100

    string and int/float to tuple

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python3
+"""This module contains the function to_kv(k, v)"""
+from typing import Tuple, Union
+
+
+def to_kv (k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """the function returns a tuple."""
+    return k, v**2
