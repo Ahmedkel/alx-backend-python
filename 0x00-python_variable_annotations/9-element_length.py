ed7813bff357e49cd16baac6489d46408d9529a8
Ahmedkel <ahmedkel@gmail.com>
2024-01-11 17:54:55 +0100

    Annotate a function parameters

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python3
+"""This module contains the function"""
+from typing import Iterable, Sequence, List, Union, Tuple
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """the function returns multiplication in float"""
+    return [(i, len(i)) for i in lst]
