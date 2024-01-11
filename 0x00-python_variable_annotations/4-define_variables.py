bf89a107eccb7f2db29783a9663e20e5ff617b2e
Ahmedkel <ahmedkel@gmail.com>
2024-01-11 16:21:25 +0100

    fix

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python3
-"""
-This module contains the Define and
-annotation with the specified values
-"""
+"""the annotation with the specified values"""
 
 
 a: int = 1
 pi: float = 3.14
 i_understand_annotations: bool = True
-school: str = "Holberton"
+school: str = 'Holberton'
