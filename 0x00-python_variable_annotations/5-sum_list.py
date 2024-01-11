ea4770c795b12d20a9968421a6b39316a530a2b5
Ahmedkel <ahmedkel@gmail.com>
2024-01-11 16:39:59 +0100

    using external model

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """This module contains the function sum_list(a, b)"""
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """the function returns sum of list"""
     return sum(input_list)
