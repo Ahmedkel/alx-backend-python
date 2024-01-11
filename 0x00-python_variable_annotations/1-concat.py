5acda60eb591a4caca512778967561998d853e26
Ahmedkel <ahmedkel@gmail.com>
2024-01-11 15:56:12 +0100

    type-annotated function concat

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,1 +1,7 @@
 #!/usr/bin/env python3
+"""This module contains the function concat(srt1, str2)"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """the function returns concatenation"""
+    return str1 + '' + str2
