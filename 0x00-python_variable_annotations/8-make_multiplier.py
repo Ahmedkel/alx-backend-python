7b37536a4f98875ead7f0c561affa77f6e0ce047
Ahmedkel <ahmedkel@gmail.com>
2024-01-11 17:48:01 +0100

    multiplication in float

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python3
+"""
+This module contains the function
+make_multiplier(multiplier: str)
+"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: str) -> Callable[[float], float]:
+    """the function returns multiplication in float"""
+    def x(f: float) -> float:
+        return float(f * multiplier)
+
+    return x
