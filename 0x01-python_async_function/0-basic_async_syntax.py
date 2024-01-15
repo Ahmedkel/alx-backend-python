a0daf71b99786a4eeb411f30ea38d6372633131d
Ahmedkel <ahmedkel@gmail.com>
2024-01-15 14:56:27 +0100

    async and random?

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python3
+"""This module contains the function"""
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """
+    Asynchronous coroutine that waits for a random
+    delay between 0 and max_delay seconds.
+
+    Parameters:
+    - max_delay (int): The maximum delay in seconds (default is 10).
+
+    Returns:
+    - float: The random delay.
+    """
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(max_delay)
+    return delay
